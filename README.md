910105a4e11501c5af50a03786657b6d85959505
victoriatsitskishvili <victoria.tsitskishvili@gmail.com>
2021-11-26 15:50:54 -0500

    moving the ready project from the local repo

--- README.md
+++ README.md
@@ -1,2 +1,111 @@
 # My-Weather-Dashboard-Web-Application
 API - Weather Dashboard
+1. HTML
+1) Creating a simple html skeleton - adding English as an language (lang="en"); translating machine code into human-readable text and vice versa to be displayed in the browser (<meta charset="utf-8">); controlling dimensions and scaling (<meta name="viewport" content="width=device-width, initial-scale=1.0">); telling Microsoft Internet Explorer users to use the latest rendering engine (<meta http-equiv="X-UA-Compatible" content="ie=edge" />)
+2) Linking an empty (for now) css file - style.css
+3) Creating a title - Weather Dashboard
+4) Accroding the ss which was provided, we need to create a header that will be centered, with white text - according to the Bootstrap documantion:
+- Creating the container-fluid class - provides a full width container, spanning the entire width of the viewport
+- Padding utilities setting up to 2 
+- Setting the background colour to dark - .bg-dark
+5) Adding H1 - Weather Dashboard 
+6) Setting up max and width resolutions by creating a div class
+7) Creating a div class - row 
+8) Creating a new class - left - to create a form (grid) : for a large screen, adding a padding-bottom - 3 
+9) Creating a h2 element - Search for a City by using an id : margin top 1
+10) Creating an Id and class for a form - using bootstrap documentation: 
+- form-inline class to display a series of labels, form controls, and buttons on a single horizontal row
+- form-group provides a flexible class that encourages proper grouping of labels, controls, optional help text, and form validation messaging. 
+- input group to add text, buttons, or button groups on either side of textual inputs, custom selects, and custom file inputs. - In this case : "text","search-input","San Diego","form-heading", "today forecast"
+- Adding a bootstrap class: .input-group-append
+- Adding a search button 
+11) Using Aria - https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/ARIA_Live_Regions
+12) Using day.js to format dates
+13) Adding js file 
+
+2. CSS
+- Set up the line height for all browsers - line-height
+- Make sure that the font size will not adjust in iOS - When an element containing text uses 100% of the screen's width, the algorithm increases its text size, but without modifying the layout. The text-size-adjust property allows web authors to disable or modify this behavior, as web pages designed with small screens in mind do not need it.
+Sections styling:
+- Setting up up margin to 0 (all browsers)
+- For a main element - Displays an element as a block element. It starts on a new line, and takes up the whole width - using display block 
+- Setting up a font size and margin for H1 element (section and article) for different browsers 
+- For the grouping elements we are adding the box size and hight 
+- Adding an overflow property - Default. The overflow is not clipped. The content renders outside the element's box (what happens to content that is too big to fit into an area.)
+- Using a pre tag - Preformatted Text element - text will be presented exactly as written in the HTML file. - using Monospaced font; and font size - 1em is equal to the current font size. The default text size in browsers is 16px. So, the default size of 1em is 16px.
+Semantic styling:
+- Setting up the background color of an element - transparent - Specifies that the background color should be transparent. This is default (to remove the gray background color)
+- <abbr>: The Abbreviation element (https://developer.mozilla.org/en-US/docs/Web/HTML/Element/abbr) - to remove the bottom border, The text-decoration property specifies the decoration added to text 
+- Adding the font weight - https://developer.mozilla.org/en-US/docs/Web/CSS/font-weight
+- <code>	Defines a piece of computer code
+-  <kbd> tag is used to define keyboard input. 
+- <samp>	Defines sample output from a computer program
+Setting up the font and size 
+- Adding the font size for the all browsers - small
+- <sup> tag defines superscript text.
+- <sub> HTML element specifies inline text which should be displayed as subscript for solely typographical reasons.
+- Preventing from affecting the line height (all browsers)
+- Removing a border on an imges
+- On the forms - changing the font style, setting up a margin to 0
+- Making sure that overflow is showing
+- The text-transform property controls the capitalization of text - No capitalization. The text renders as it is. This is default
+- Style clickable typy - The element is drawn like a button
+- Removing the inner border and padding 
+- Restoring the focus styles - -moz-focusring
+- Setting up a correct padding 
+- Correcting the text wrapping, color, removing the padding 
+-  Adding the correct vertical alignment - https://developer.mozilla.org/en-US/docs/Web/CSS/vertical-align
+- Removing the default vertical scrollbar
+- Setting up the correct box size and removing the padding 
+- Cursor style - https://developer.mozilla.org/en-US/docs/Web/CSS/::-webkit-inner-spin-button
+- Adding outline style 
+- Removing the inner padding
+- Styling the clickable types and changing font properties to inherit
+- Adding the correct display property 
+- Creating height and width utilities styles 
+- Adding margins on all sides 
+- Adding margin bottoms 
+- Adding margin tops 
+- Adding margin - right
+- Adding margin - left 
+- Adding margin - x axis
+- Adding margin - y axis
+- Adding padding on all sides
+- Adding padding bottoms 
+- Adding padding tops 
+- Adding padding - right
+- Adding padding - left 
+- Adding padding - x axis
+- Adding padding - y axis
+- Setting up styles for background utilities 
+- Setting up styles for display properties 
+- Setting up flex content properties 
+- Setting up the style to make sure ut works with small, medium, large and extra large devices 
+
+3. JS
+- Setting up global variables : API key, API URL and Search History 
+- Referencing all the DOM elements (document.querySelector)
+- Adding timezone plugs - dayjs.extend
+- Using a function to display the previous search - renderSearchHistory
+- Runing a for loop to show the most recent search at the top
+- When you click on the city from your search history (list on the left) it should show the informaion on hte main screen (data-search)
+- Creating a function to update the search history in local storage and display 
+- If nothing is in search - return the function 
+- In order to get search history from the local storage we need to run a function - initSearchHistory
+- To display the current weatehr we need to run the function to display the data from the API - renderCurrentWeather(city, weather, timezone)
+- Creating vars to store the data 
+- Running a function to display forecast card (from weatehr API) - renderForecastCard
+- Setting up vars for the data that comes from api 
+- Card - we need to create all the elements 
+- Adding all the content to the elements - °F, MPH, % etc 
+- Creating a function to display forecast for 5 days 
+- Creating unix timestamps for start and end of the forecast 
+- Runing a for loop - timezone
+- Creating a function (fetchWeather) - to get the weatehr data for the given specific location, then we are calling this function to display the data 
+- Adding another function if location is not added
+- Returning the function if there is nothing in the search box 
+- Returning the function if the element is not showing under search history 
+- 
+
+
+
---
+++ script.js
@@ -0,0 +1,251 @@
+// Setting up Global vars //
+var searchHistory = [];
+var weatherApiRootUrl = 'https://api.openweathermap.org';
+var weatherApiKey = 'd91f911bcf2c0f925fb6535547a5ddc9';
+
+// reference for DOM//
+var searchForm = document.querySelector('#search-form');
+var searchInput = document.querySelector('#search-input');
+var todayContainer = document.querySelector('#today');
+var forecastContainer = document.querySelector('#forecast');
+var searchHistoryContainer = document.querySelector('#history');
+
+// timezone plugins //
+dayjs.extend(window.dayjs_plugin_utc);
+dayjs.extend(window.dayjs_plugin_timezone);
+
+// Displaying search history //
+function renderSearchHistory() {
+  searchHistoryContainer.innerHTML = '';
+
+  // To show most recent results at the top of the list //
+  for (var i = searchHistory.length - 1; i >= 0; i--) {
+    var btn = document.createElement('button');
+    btn.setAttribute('type', 'button');
+    btn.setAttribute('aria-controls', 'today forecast');
+    btn.classList.add('history-btn', 'btn-history');
+
+    // Click on the city from search history funciton //
+    btn.setAttribute('data-search', searchHistory[i]);
+    btn.textContent = searchHistory[i];
+    searchHistoryContainer.append(btn);
+  }
+}
+
+// Update history in local storage and update history that displays on the screen//
+function appendToHistory(search) {
+  // Return the function//
+  if (searchHistory.indexOf(search) !== -1) {
+    return;
+  }
+  searchHistory.push(search);
+
+  localStorage.setItem('search-history', JSON.stringify(searchHistory));
+  renderSearchHistory();
+}
+
+// Getting the history from the search from the local storage function //
+function initSearchHistory() {
+  var storedHistory = localStorage.getItem('search-history');
+  if (storedHistory) {
+    searchHistory = JSON.parse(storedHistory);
+  }
+  renderSearchHistory();
+}
+
+// Displaying the weather data from the OpenWeather api //
+function renderCurrentWeather(city, weather, timezone) {
+  var date = dayjs().tz(timezone).format('M/D/YYYY');
+
+  // To store the data //
+  var tempF = weather.temp;
+  var windMph = weather.wind_speed;
+  var humidity = weather.humidity;
+  var uvi = weather.uvi;
+  var iconUrl = `https://openweathermap.org/img/w/${weather.weather[0].icon}.png`;
+  var iconDescription = weather.weather[0].description || weather[0].main;
+
+  var card = document.createElement('div');
+  var cardBody = document.createElement('div');
+  var heading = document.createElement('h2');
+  var weatherIcon = document.createElement('img');
+  var tempEl = document.createElement('p');
+  var windEl = document.createElement('p');
+  var humidityEl = document.createElement('p');
+  var uvEl = document.createElement('p');
+  var uviBadge = document.createElement('button');
+
+  card.setAttribute('class', 'card');
+  cardBody.setAttribute('class', 'card-body');
+  card.append(cardBody);
+
+  heading.setAttribute('class', 'h3 card-title');
+  tempEl.setAttribute('class', 'card-text');
+  windEl.setAttribute('class', 'card-text');
+  humidityEl.setAttribute('class', 'card-text');
+
+  heading.textContent = `${city} (${date})`;
+  weatherIcon.setAttribute('src', iconUrl);
+  weatherIcon.setAttribute('alt', iconDescription);
+  weatherIcon.setAttribute('class', 'weather-img');
+  heading.append(weatherIcon);
+  tempEl.textContent = `Temp: ${tempF}°F`;
+  windEl.textContent = `Wind: ${windMph} MPH`;
+  humidityEl.textContent = `Humidity: ${humidity} %`;
+  cardBody.append(heading, tempEl, windEl, humidityEl);
+
+  uvEl.textContent = 'UV Index: ';
+  uviBadge.classList.add('btn', 'btn-sm');
+
+  if (uvi < 3) {
+    uviBadge.classList.add('btn-success');
+  } else if (uvi < 7) {
+    uviBadge.classList.add('btn-warning');
+  } else {
+    uviBadge.classList.add('btn-danger');
+  }
+
+  uviBadge.textContent = uvi;
+  uvEl.append(uviBadge);
+  cardBody.append(uvEl);
+
+  todayContainer.innerHTML = '';
+  todayContainer.append(card);
+}
+
+// Forecast card function //
+function renderForecastCard(forecast, timezone) {
+  // Setting up vars //
+  var unixTs = forecast.dt;
+  var iconUrl = `https://openweathermap.org/img/w/${forecast.weather[0].icon}.png`;
+  var iconDescription = forecast.weather[0].description;
+  var tempF = forecast.temp.day;
+  var { humidity } = forecast;
+  var windMph = forecast.wind_speed;
+
+  // Card elements //
+  var col = document.createElement('div');
+  var card = document.createElement('div');
+  var cardBody = document.createElement('div');
+  var cardTitle = document.createElement('h5');
+  var weatherIcon = document.createElement('img');
+  var tempEl = document.createElement('p');
+  var windEl = document.createElement('p');
+  var humidityEl = document.createElement('p');
+
+  col.append(card);
+  card.append(cardBody);
+  cardBody.append(cardTitle, weatherIcon, tempEl, windEl, humidityEl);
+
+  col.setAttribute('class', 'col-md');
+  col.classList.add('five-day-card');
+  card.setAttribute('class', 'card bg-primary h-100 text-white');
+  cardBody.setAttribute('class', 'card-body p-2');
+  cardTitle.setAttribute('class', 'card-title');
+  tempEl.setAttribute('class', 'card-text');
+  windEl.setAttribute('class', 'card-text');
+  humidityEl.setAttribute('class', 'card-text');
+
+  // Content for the elements //
+  cardTitle.textContent = dayjs.unix(unixTs).tz(timezone).format('M/D/YYYY');
+  weatherIcon.setAttribute('src', iconUrl);
+  weatherIcon.setAttribute('alt', iconDescription);
+  tempEl.textContent = `Temp: ${tempF} °F`;
+  windEl.textContent = `Wind: ${windMph} MPH`;
+  humidityEl.textContent = `Humidity: ${humidity} %`;
+
+  forecastContainer.append(col);
+}
+
+// Displaying 5 days forecast //
+function renderForecast(dailyForecast, timezone) {
+  // Unix timestamps (start and end of 5 day forecast) //
+  var startDt = dayjs().tz(timezone).add(1, 'day').startOf('day').unix();
+  var endDt = dayjs().tz(timezone).add(6, 'day').startOf('day').unix();
+
+  var headingCol = document.createElement('div');
+  var heading = document.createElement('h4');
+
+  headingCol.setAttribute('class', 'col-12');
+  heading.textContent = '5-Day Forecast:';
+  headingCol.append(heading);
+
+  forecastContainer.innerHTML = '';
+  forecastContainer.append(headingCol);
+  for (var i = 0; i < dailyForecast.length; i++) {
+    if (dailyForecast[i].dt >= startDt && dailyForecast[i].dt < endDt) {
+      renderForecastCard(dailyForecast[i], timezone);
+    }
+  }
+}
+
+function renderItems(city, data) {
+  renderCurrentWeather(city, data.current, data.timezone);
+  renderForecast(data.daily, data.timezone);
+}
+
+// Getting the data for the current search and display it //
+function fetchWeather(location) {
+  var { lat } = location;
+  var { lon } = location;
+  var city = location.name;
+  var apiUrl = `${weatherApiRootUrl}/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=${weatherApiKey}`;
+
+  fetch(apiUrl)
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      renderItems(city, data);
+    })
+    .catch(function (err) {
+      console.error(err);
+    });
+}
+
+function fetchCoords(search) {
+  var apiUrl = `${weatherApiRootUrl}/geo/1.0/direct?q=${search}&limit=5&appid=${weatherApiKey}`;
+
+  fetch(apiUrl)
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      if (!data[0]) {
+        alert('Location not found');
+      } else {
+        appendToHistory(search);
+        fetchWeather(data[0]);
+      }
+    })
+    .catch(function (err) {
+      console.error(err);
+    });
+}
+
+function handleSearchFormSubmit(e) {
+  // Return the function if there is nothing in the search
+  if (!searchInput.value) {
+    return;
+  }
+
+  e.preventDefault();
+  var search = searchInput.value.trim();
+  fetchCoords(search);
+  searchInput.value = '';
+}
+
+function handleSearchHistoryClick(e) {
+  //If the element is not under search history, dont call search 
+  if (!e.target.matches('.btn-history')) {
+    return;
+  }
+
+  var btn = e.target;
+  var search = btn.getAttribute('data-search');
+  fetchCoords(search);
+}
+
+initSearchHistory();
+searchForm.addEventListener('submit', handleSearchFormSubmit);
+searchHistoryContainer.addEventListener('click', handleSearchHistoryClick);
